ac1675bd1a4ac5d6de3cebfe5fdfceb84cac7dc9
Muhammad Fazal <38839909+fazaltech@users.noreply.github.com>
2022-02-13 22:46:02 +0500

    Add Tables Model In AppDbContext

--- eTickets/Data/AppDbContext.cs
+++ eTickets/Data/AppDbContext.cs
@@ -22,7 +22,16 @@ namespace eTickets.Data
             am.MovieId
             });
 
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Movie).WithMany(am => am.Actor_Movies).HasForeignKey(m => m.MovieId);
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Actor).WithMany(am => am.Actor_Movies).HasForeignKey(m => m.ActorId);
+
             base.OnModelCreating(modelBuilder);
         }
+
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+        public DbSet<Actor_Movie> Actor_Movies { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
     }
 }
