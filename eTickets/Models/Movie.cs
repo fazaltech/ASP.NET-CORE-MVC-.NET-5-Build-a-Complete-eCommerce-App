b1e398a5e904213b681f7840359f730e2ea5cf8c
Muhammad Fazal <38839909+fazaltech@users.noreply.github.com>
2022-02-13 21:30:57 +0500

    Add Producer and Relationship

--- eTickets/Models/Movie.cs
+++ eTickets/Models/Movie.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -33,7 +34,14 @@ namespace eTickets.Models
 
         //Cinema
         public int CinemaId { get; set; }
+        [ForeignKey("CinemaId")]
 
         public Cinema Cinema { get; set; }
+
+        //Producer
+        public int ProducerId { get; set; }
+        [ForeignKey("ProducerId")]
+
+        public Producer Producer { get; set; }
     }
 }
