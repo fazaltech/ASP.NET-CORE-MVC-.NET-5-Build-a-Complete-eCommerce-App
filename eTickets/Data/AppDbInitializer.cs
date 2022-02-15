789e9820ad1d1dc82155d1f6ad6cf6a0db4ff545
Muhammad Fazal <38839909+fazaltech@users.noreply.github.com>
2022-02-15 21:55:12 +0500

    Add AppDbInitializer

---
+++ eTickets/Data/AppDbInitializer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data
+{
+    public class AppDbInitializer
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder  )
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope()) 
+            {
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+
+                context.Database.EnsureCreated();
+            }
+        }
+    }
+}
