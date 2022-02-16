bf8648d2ec2f1641124a1426a549e0ee4f83ec2a
Muhammad Fazal <38839909+fazaltech@users.noreply.github.com>
2022-02-16 22:51:45 +0500

    update AppDbInitializer

--- eTickets/Models/Movie.cs
+++ eTickets/Models/Movie.cs
@@ -18,7 +18,7 @@ namespace eTickets.Models
 
         public string Description { get; set; }
         
-        public string Price { get; set; }
+        public double Price { get; set; }
         
         public string ImageURL { get; set; }
 
