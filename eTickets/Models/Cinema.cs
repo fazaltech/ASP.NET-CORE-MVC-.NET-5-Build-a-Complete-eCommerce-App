02b20e90d1f4064eb7d4021515d7d981050ef96b
Muhammad Fazal <38839909+fazaltech@users.noreply.github.com>
2022-03-30 19:18:58 +0500

    Adding the cinemas view

--- eTickets/Models/Cinema.cs
+++ eTickets/Models/Cinema.cs
@@ -12,8 +12,13 @@ namespace eTickets.Models
         [Key]
         public int Id { get; set; }
 
+        [Display(Name ="Cinema Logo")]
         public string Logo { get; set; }
+
+        [Display(Name = "Cinema Name")]
         public string Name { get; set; }
+
+        [Display(Name = "Cinema Description")]
         public string Description { get; set; }
 
         //Relationships
