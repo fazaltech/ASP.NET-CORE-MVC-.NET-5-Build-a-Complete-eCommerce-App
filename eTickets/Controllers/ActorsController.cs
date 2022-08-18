c8896285d042159238ae52277b54136fdb37bb39
Muhammad Fazal <38839909+fazaltech@users.noreply.github.com>
2022-08-18 21:38:31 +0500

    Model Validation - Adding data to database

--- eTickets/Controllers/ActorsController.cs
+++ eTickets/Controllers/ActorsController.cs
@@ -1,5 +1,6 @@
 ﻿using eTickets.Data;
 using eTickets.Data.Services;
+using eTickets.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -25,6 +26,16 @@ namespace eTickets.Controllers
         public async Task<IActionResult> Create() 
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("FullName,ProfilePictureURL,Bio")] Actor actor) 
+        {
+            if (!ModelState.IsValid) 
+            {
+                return View(actor);
+            }
+            _service.Add(actor);
+            return RedirectToAction(nameof(Index));
         }
 
     }
--- eTickets/Data/Services/ActorsService.cs
+++ eTickets/Data/Services/ActorsService.cs
@@ -17,7 +17,10 @@ namespace eTickets.Data.Services
         }
         public void Add(Actor actor)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+
+            _context.Actors.Add(actor);
+            _context.SaveChanges();
         }
 
         public void Delete(int id)
