625bbbc6490936d11d8f5d4834e302bc13b6ce9d
Muhammad Fazal <38839909+fazaltech@users.noreply.github.com>
2022-08-21 22:19:45 +0500

    Adding EntityBaseRepository files
    Adding EntityBaseRepository files
    &
    GetAllAsync() & GetByIdAsync() base methods

---
+++ eTickets/Data/Base/EntityBaseRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Base
+{
+    public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+
+        private readonly AppDbContext _context;
+
+        public EntityBaseRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public Task AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            var result = await _context.Set<T>().ToListAsync();
+            return result;
+            //throw new NotImplementedException();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            var result = await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
+            return result;
+        }
+      
+
+       
+
+
+        public Task<T> UpdateAsync(int id, T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
