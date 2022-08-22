b655f11313a85c5ea2e566aba3bcbcf040e155de
Muhammad Fazal <38839909+fazaltech@users.noreply.github.com>
2022-08-22 23:13:10 +0500

    ProducersService configuration with EntityBaseRepository

---
+++ eTickets/Data/Services/ProducersService.cs
@@ -0,0 +1,19 @@
+﻿using eTickets.Data.Base;
+using eTickets.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Services
+{
+    public class ProducersService: EntityBaseRepository<Producer>, IProducersService
+    {
+        private readonly AppDbContext _context;
+
+        public ProducersService(AppDbContext context) : base(context)
+        {
+
+        }
+    }
+}
--- eTickets/Startup.cs
+++ eTickets/Startup.cs
@@ -32,6 +32,7 @@ namespace eTickets
 
 
             services.AddScoped<IActorsService, ActorsService>();
+            services.AddScoped<IProducersService, ProducersService>();
 
             services.AddControllersWithViews();
         }
