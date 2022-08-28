7e4823bda32aed260d8b8d92e575317d504c14de
Muhammad Fazal <38839909+fazaltech@users.noreply.github.com>
2022-08-28 15:08:53 +0500

    Getting cinema details & Updating existing cinema

--- eTickets/Controllers/CinemasController.cs
+++ eTickets/Controllers/CinemasController.cs
@@ -24,6 +24,7 @@ namespace eTickets.Controllers
             return View(allCinemas);
         }
 
+        //Get: Cinema/Create
         public IActionResult Create() 
         {
             return View();
@@ -33,6 +34,32 @@ namespace eTickets.Controllers
         {
             if (!ModelState.IsValid) return View(cinema);
             await _service.AddAsync(cinema);
+            return RedirectToAction(nameof(Index));
+        }
+
+        //Get: Cinema/Detail/1
+        public async Task<IActionResult> Details(int id) 
+        {
+            var cinemaDetails = await _service.GetByIdAsync(id);
+            if (cinemaDetails == null) return View("NotFound");
+            return View(cinemaDetails);
+        }
+
+
+        //Get: Cinema/Edit/1
+        public async Task<IActionResult> Edit(int id)
+        {
+            var cinemaDetails = await _service.GetByIdAsync(id);
+            if (cinemaDetails == null) return View("NotFound");
+            return View(cinemaDetails);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Logo,Name,Description")] Cinema cinema)
+        {
+            if (!ModelState.IsValid) return View(cinema);
+            await _service.UpdateAsync(id, cinema);
             return RedirectToAction(nameof(Index));
         }
     }
