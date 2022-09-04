8eaa891268470a96fd3bbe0788624a46555c9c9a
Muhammad Fazal <38839909+fazaltech@users.noreply.github.com>
2022-09-04 22:38:40 +0500

    Adding new movie to database

--- eTickets/Data/Services/IMoviesService.cs
+++ eTickets/Data/Services/IMoviesService.cs
@@ -12,5 +12,7 @@ namespace eTickets.Data.Services
     {
         Task<Movie> GetMovieByIdAsync(int id);
         Task<NewMovieDropdownsVM> GetNewMovieDropdownsValues();
+
+        Task AddNewMovieAsync(NewMovieVM data);
     }
 }
