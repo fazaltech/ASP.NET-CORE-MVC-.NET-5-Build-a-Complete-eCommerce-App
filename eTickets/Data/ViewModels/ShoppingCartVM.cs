68f8bfd8657fb73d53afa5feba3ea46ca6c73c17
Muhammad Fazal <38839909+fazaltech@users.noreply.github.com>
2022-10-16 13:57:24 +0500

    Adding OrdersController file

---
+++ eTickets/Data/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,15 @@
+﻿using eTickets.Data.Cart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.ViewModels
+{
+    public class ShoppingCartVM
+    {
+        public ShoppingCart ShoppingCart { get; set; }
+
+        public double ShoppingCartTotal { get; set; }
+    }
+}
