11830d96694c67f7767d959fed80e94fabc0e896
Muhammad Fazal <38839909+fazaltech@users.noreply.github.com>
2022-10-16 23:32:57 +0500

    Add OrdersService Class

---
+++ eTickets/Data/Services/IOrdersService.cs
@@ -0,0 +1,15 @@
+﻿using eTickets.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Services
+{
+  public  interface IOrdersService
+    {
+        Task StoreOrderAsync(List<ShoppingCartItem> item, string userId, string userEmailAddress);
+
+        Task<List<Order>> GetOrdersByUserIdAsync(string userId);
+    }
+}
---
+++ eTickets/Data/Services/OrdersService.cs
@@ -0,0 +1,26 @@
+﻿using eTickets.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Services
+{
+    public class OrdersService : IOrdersService
+    {
+        private readonly AppDbContext _context;
+        public OrdersService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public Task<List<Order>> GetOrdersByUserIdAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StoreOrderAsync(List<ShoppingCartItem> item, string userId, string userEmailAddress)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
