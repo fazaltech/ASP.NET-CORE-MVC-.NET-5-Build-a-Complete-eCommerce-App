2a3c54fdabca11a92df584d4b50501faa9f2f633
Muhammad Fazal <38839909+fazaltech@users.noreply.github.com>
2022-10-22 19:54:16 +0500

    Signing out users

--- eTickets/Controllers/AccountController.cs
+++ eTickets/Controllers/AccountController.cs
@@ -78,5 +78,12 @@ namespace eTickets.Controllers
             return View("RegisterCompleted");
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Logout() 
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Movies");
+        }
+
     }
 }
